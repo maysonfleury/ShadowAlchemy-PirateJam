23d5d97f9b7e6595be0417392cb9f24ba4b545c1
maochite <mao@jpkerrigan.dev>
2024-07-22 01:43:40 -0400

    GameManager + AudioManager

---
+++ Assets/Scripts/Static/GameManager.cs
@@ -0,0 +1,78 @@
+using System;
+using UnityEngine;
+
+
+public class GameManager : PersistentSingleton<GameManager> 
+{
+    public enum GameState
+    {
+        StartState = 0,
+        LoadState = 1,
+        IntroState = 2,
+        GameState = 3,
+        EndState = 4,
+    }
+
+
+    public static event Action<GameState> OnBeforeStateChanged;
+    public static event Action<GameState> OnAfterStateChanged;
+
+    public GameState State { get; private set; }
+
+
+    //void Start() => ChangeState(GameState.StartState);
+
+    public void ChangeState(GameState newState) {
+        OnBeforeStateChanged?.Invoke(newState);
+
+        State = newState;
+        switch (newState) {
+            case GameState.StartState:
+                HandleStarting();
+                break;
+            case GameState.LoadState:
+                HandleLoadScene();
+                break;
+            case GameState.IntroState:
+                HandleIntroScene();
+                break;
+            case GameState.GameState:
+                HandleGameScene();
+                break;
+            case GameState.EndState:
+                HandleEndScene();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+
+        OnAfterStateChanged?.Invoke(newState);
+        
+        Debug.Log($"New state: {newState}");
+    }
+
+    private void HandleStarting() 
+    {
+        ChangeState(GameState.LoadState);
+    }
+
+    private void HandleLoadScene() 
+    {
+        ChangeState(GameState.IntroState);
+    }
+
+    private void HandleIntroScene() 
+    {
+        ChangeState(GameState.GameState);
+    }
+
+    private void HandleGameScene() 
+    {
+        ChangeState(GameState.EndState);
+    }
+
+    private void HandleEndScene()
+    {
+
+    }
+}
