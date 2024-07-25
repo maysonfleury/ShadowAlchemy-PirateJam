a9e3afed67d3edbd1e408dd6f8b46acddf643efa
maochite <mao@jpkerrigan.dev>
2024-07-25 19:11:56 -0400

    Basic boss state machine + charge attack

--- Assets/Scripts/Boss/BossController.cs
+++ Assets/Scripts/Boss/BossController.cs
@@ -13,6 +13,9 @@ namespace Boss
         Dying,
     }
 
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Collider2D))]
     public class BossController : MonoBehaviour, IEffectable
     {
 
@@ -25,16 +28,15 @@ namespace Boss
 
 
         [field: Space]
-        [field: Header("Boss Pivots")]
-        [field: SerializeField] public Transform AttackPivot { get; private set; }
+        [field: Header("Boss Attack Points")]
         [field: SerializeField] public Transform FirePoint { get; private set; }
-        [field: SerializeField] public Transform SightPivot { get; private set; }
+        [field: SerializeField] public Collider2D AttackBox { get; private set; }
 
         [field: Space]
         [field: Header("Boss Sensors")]
 
         [field: SerializeField] public Collider2D GroundSensor { get; private set; }
-        [field: SerializeField] public Collider2D AttackSensor { get; private set; }
+        [field: SerializeField] public Collider2D TargetSensor { get; private set; }
 
         [field: Space]
         [field: Header("Attack Components")]
@@ -48,22 +50,31 @@ namespace Boss
         private ContactFilter2D targetFilter;
 
         private readonly Collider2D[] sensorResults = new Collider2D[50];
-        private float currentAttackCooldown = 0;
         private float attackAnimationLength = 0;
         private float currentAttackAnimationTime = 0;
 
         private int currentAttackPattern = -1;
         private int currentAttackIndex = -1;
-        private int lastAttackPattern = -1;
+
+        const float stabilizeTime = 1;
+        private float currentStabilizingTime = 0;
+
+        const float attackCooldownTime = 1;
+        private float currentAttackCooldown = 0;
+
+        private bool attackBoxEnabled = false;
+        private EffectSO currentAttackEffect = null;
 
         public string[][] AttackPatterns = new string[][]
         {
-            new string[] { "Stab", "Slash", "Charge" },
-            new string[] { "Slash", "Fissure", "Charge" },
-            new string[] { "BattleShout", "Slash", "Stab" },
-            new string[] { "LeapSlam", "Charge", "LeapSlam" },
-            new string[] { "Battleshout", "Stomp", "Charge" },
-            new string[] { "Fireball", "LeapSlam", "Stab" },
+            new string[] { "Charge", "Charge", "Charge" },
+            //new string[] { "Stab", "Slash", "Charge" },
+            //new string[] { "Stab", "Slash", "Stab" },
+            //new string[] { "Slash", "Fissure", "Charge" },
+            //new string[] { "BattleShout", "Slash", "Stab" },
+            //new string[] { "LeapSlam", "Charge", "LeapSlam" },
+            //new string[] { "Battleshout", "Stomp", "Charge" },
+            //new string[] { "Fireball", "LeapSlam", "Stab" },
         };
 
 
@@ -120,13 +131,48 @@ namespace Boss
         private void FixedUpdate()
         {
             if (!GroundCheck())
+            {
+                currentStabilizingTime = stabilizeTime + Time.time;
                 return;
+            }
+
+            else if(currentStabilizingTime > Time.time)
+            {
+                return;
+            }
 
             switch (BossState)
             {
                 case BossState.Idle:
+                    if (TargetProximityCheck(out Collider2D target))
+                    { 
+                        if (!IsPointInFront(target.transform.position))
+                        {
+                            FlipCharacter();
+                        }
+
+                        if (currentAttackCooldown < Time.time)
+                        {
+                            ChangeState(BossState.Attacking);
+                        }
+                    }
+
                     break;
                 case BossState.Attacking:
+                    if(AttackProximityCheck(out target))
+                    {
+                        if(target.TryGetComponent(out IEffectable effectable))
+                        {
+                            effectable.ApplyEffect(currentAttackEffect);
+                        }
+                    }
+
+                    if (currentAttackAnimationTime < Time.time)
+                    {
+                        OnAttackComplete();
+                        ChangeState(BossState.Idle);
+                    }
+
                     break;
                 case BossState.Dying:
                     break;
@@ -140,6 +186,7 @@ namespace Boss
                 case BossState.Idle:
                     break;
                 case BossState.Attacking:
+                    Attack();
                     break;
                 case BossState.Dying:
                     break;
@@ -150,32 +197,35 @@ namespace Boss
 
         private void Attack()
         {
-            if (currentAttackPattern < 0 || currentAttackPattern - 1 > AttackPatterns.Length)
-            {
-                int randomNumber;
 
-                do
-                {
-                    randomNumber = UnityEngine.Random.Range(0, AttackPatterns.Length - 1);
-                }
-                while (lastAttackPattern != randomNumber);
+            currentAttackIndex++;
+            if ((currentAttackPattern < 0 || currentAttackPattern - 1 > AttackPatterns.Length)
+                || currentAttackIndex > AttackPatterns[currentAttackPattern].Length - 1)
+            {
+                currentAttackPattern = UnityEngine.Random.Range(0, AttackPatterns.Length - 1);
+                currentAttackIndex = 0;
             }
 
-            else
-            {
-                currentAttackIndex++;
-                if (currentAttackIndex > AttackPatterns[currentAttackPattern].Length)
-                {
-                    lastAttackPattern = currentAttackPattern;
-                    currentAttackPattern = -1;
-                    return;
-                }
+            attackAnimationLength = GetClipLength(AttackPatterns[currentAttackPattern][currentAttackIndex]);
+            currentAttackAnimationTime = attackAnimationLength + BossData.AttackCooldown + Time.time;
+            Animator.SetTrigger(AttackPatterns[currentAttackPattern][currentAttackIndex]);
+        }
 
-                else
-                {
-                    Animator.SetTrigger(AttackPatterns[currentAttackPattern][currentAttackIndex]);
-                }
-            }
+        private void OnAttackComplete()
+        {
+            DisableAttackBox();
+        }
+
+        private void EnableAttackBox(EffectSO effectSO)
+        {
+            attackBoxEnabled = true;
+            currentAttackEffect = effectSO;
+        }
+
+        private void DisableAttackBox()
+        {
+            attackBoxEnabled = false;
+            currentAttackEffect = null;
         }
 
         private bool GroundCheck()
@@ -183,9 +233,14 @@ namespace Boss
             return ColliderCheck(GroundSensor, groundFilter, out _);
         }
 
+        private bool TargetProximityCheck(out Collider2D hitCollider)
+        {
+            return ColliderCheck(TargetSensor, targetFilter, out hitCollider);
+        }
+
         private bool AttackProximityCheck(out Collider2D hitCollider)
         {
-            return ColliderCheck(AttackSensor, targetFilter, out hitCollider);
+            return ColliderCheck(AttackBox, targetFilter, out hitCollider);
         }
 
         private bool ColliderCheck(Collider2D sensor, ContactFilter2D filter, out Collider2D hitCollider)
@@ -206,9 +261,68 @@ namespace Boss
             return false;
         }
 
+        private Vector2 GetDirection(Vector2 startPoint, Vector2 endPoint)
+        {
+            return endPoint - startPoint;
+        }
+
+        private bool IsPointInFront(Vector2 point)
+        {
+            Vector2 direction = GetDirection(transform.position, point).normalized;
+
+            if ((direction.x > 0 && HorizontalFacing > 0)
+                || direction.x < 0 && HorizontalFacing < 0)
+            {
+                return true;
+            }
+
+            else return false;
+        }
+
+        protected void FlipCharacter()
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+
+            HorizontalFacing = -HorizontalFacing;
+        }
+
+        private float GetClipLength(string clipName)
+        {
+            RuntimeAnimatorController ac = Animator.runtimeAnimatorController;
+            foreach (AnimationClip clip in ac.animationClips)
+            {
+                if (clip.name == clipName)
+                {
+                    return clip.length;
+                }
+            }
+            return 0f;
+        }
+
         public void ApplyEffect(EffectSO effectSO)
         {
             throw new System.NotImplementedException();
+        }
+
+        public void ApplyForce(ForceSO forceSO)
+        {
+            float new_x = HorizontalFacing * forceSO.Direction.x;
+            Vector2 velocity = new Vector2(new_x, forceSO.Direction.y).normalized * forceSO.Speed;
+            Rigidbody.AddForce(velocity, forceSO.ForceMode);
+        }
+
+        public void ApplyRelativeForce(float forward, ForceSO forceSO)
+        {
+            if (forward == 0)
+            {
+                forward = 1;
+            }
+
+            float new_x = forward * forceSO.Direction.x;
+            Vector2 velocity = new Vector2(new_x, forceSO.Direction.y).normalized * forceSO.Speed;
+            Rigidbody.AddForce(velocity, forceSO.ForceMode);
         }
 
 
