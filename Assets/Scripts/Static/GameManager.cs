e39ed3da616c274c9bbe40c4b1e939a47b41c79f
Mayson <mayson11@live.ca>
2024-08-03 14:02:30 -0400

    Fixed scene loading

--- Assets/Scripts/Static/GameManager.cs
+++ Assets/Scripts/Static/GameManager.cs
@@ -15,17 +15,47 @@ public class GameManager : PersistentSingleton<GameManager>
         EndState = 5,
     }
 
-
     public static event Action<GameState> OnBeforeStateChanged;
     public static event Action<GameState> OnAfterStateChanged;
 
     public GameState State { get; private set; }
 
+    private bool _MouseAim = false;
+
+    // called first
+    void OnEnable()
+    {
+        Debug.Log("OnEnable called");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // called second
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("OnSceneLoaded: " + scene.name + " mode: " + mode);
+        if (scene.name == "TutorialLayer" && State == GameState.LoadState)
+        {
+            if (_MouseAim)
+                SceneManager.LoadSceneAsync(3, LoadSceneMode.Additive);
+            else
+                SceneManager.LoadSceneAsync(4, LoadSceneMode.Additive);
+            State = GameState.GameState;
+        }
+    }
+
+    // called third
     void Start()
     {
         Application.targetFrameRate = 60;
         //ChangeState(GameState.StartState);
         LoadMainMenu();
+    }
+
+    // called when the game is terminated
+    void OnDisable()
+    {
+        Debug.Log("OnDisable");
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     public void ChangeState(GameState newState) {
@@ -34,19 +64,19 @@ public class GameManager : PersistentSingleton<GameManager>
         State = newState;
         switch (newState) {
             case GameState.StartState:
-                HandleStarting();
+                //HandleStarting();
                 break;
             case GameState.LoadState:
-                HandleLoadScene();
+                //HandleLoadScene();
                 break;
             case GameState.IntroState:
-                HandleIntroScene();
+                //HandleIntroScene();
                 break;
             case GameState.GameState:
-                HandleGameScene();
+                //HandleGameScene();
                 break;
             case GameState.EndState:
-                HandleEndScene();
+                //HandleEndScene();
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
@@ -57,26 +87,6 @@ public class GameManager : PersistentSingleton<GameManager>
         Debug.Log($"New state: {newState}");
     }
 
-    private void HandleStarting() 
-    {
-    }
-
-    private void HandleLoadScene() 
-    {
-    }
-
-    private void HandleIntroScene() 
-    {
-    }
-
-    private void HandleGameScene() 
-    {
-    }
-
-    private void HandleEndScene()
-    {
-    }
-
     public void ChangeScene(String scene)
     {
         SceneManager.LoadScene(scene);
@@ -84,21 +94,14 @@ public class GameManager : PersistentSingleton<GameManager>
 
     public void LoadMainMenu()
     {
-        SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
     }
 
     public void LoadGame(bool MouseAim)
     {
-        ChangeState(GameState.GameState);
-        SceneManager.UnloadSceneAsync("MainMenu");
-        if (MouseAim)
-        {
-            SceneManager.LoadSceneAsync("PlayerMouseAim", LoadSceneMode.Additive);
-        }
-        else
-        {
-            SceneManager.LoadSceneAsync("PlayerKeyAim", LoadSceneMode.Additive);
-        }
-        SceneManager.LoadSceneAsync("TutorialLayer", LoadSceneMode.Additive);
+        ChangeState(GameState.LoadState);
+        SceneManager.UnloadSceneAsync(1);
+        SceneManager.LoadSceneAsync(2);
+        _MouseAim = MouseAim;
     }
 }
